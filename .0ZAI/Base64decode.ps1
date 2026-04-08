e99c8eb35e58dee18c628820e3f133ccebe8f1d6
Yekta Kayman <yektakayman@gmail.com>
2026-04-08 13:10:59 +0300

    Gereksiz klasör ve dosyaların proje dışına alınması

--- .0ZAI/Base64decode.ps1
+++
@@ -1,3 +0,0 @@
- $base64 = Get-Content -Path "code.zipbase64.txt" -Raw
- $bytes = [System.Convert]::FromBase64String($base64.Trim())
-[System.IO.File]::WriteAllBytes("UniRide_Dokumantasyon.zip", $bytes)
